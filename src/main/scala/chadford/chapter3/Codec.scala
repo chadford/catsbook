32f7830e86866f7337cfcdee9f86b8d869d4f932
Charad Sheerajin <charad@gmail.com>
2020-04-15 01:05:04 -0500

    add Codec exercise

---
+++ src/main/scala/chadford/chapter3/Codec.scala
@@ -0,0 +1,29 @@
+package chadford.chapter3
+
+trait Codec[A] {
+  self =>
+  def encode(value: A): String
+  def decode(value: String): A
+
+  def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+    override def encode(value: B): String = self.encode(enc(value))
+    override def decode(value: String): B = dec(self.decode(value))
+  }
+}
+
+object Codec {
+  case class Box[A](value: A)
+
+  implicit val codecString = new Codec[String] {
+    override def encode(value: String): String = value
+    override def decode(value: String): String = value
+  }
+
+  implicit val codecDouble = codecString.imap[Double](_.toDouble, _.toString)
+
+  implicit def codecBox[A](implicit c: Codec[A]) = c.imap[Box[A]](Box(_), _.value)
+
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+
+  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+}
