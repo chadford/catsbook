1dc4f20d0d4796c6983605f347663c2d9deb32d8
Charad Sheerajin <charad@gmail.com>
2020-09-10 20:45:33 -0500

    add chapter 6 exercises for product of monads

---
+++ src/test/scala/chadford/chapter6/ValidatedSpec.scala
@@ -0,0 +1,9 @@
+package chadford.chapter6
+
+import org.scalatest.funspec.AnyFunSpec
+
+class ValidatedSpec extends AnyFunSpec {
+  describe("6.4.4 Exercise: Form Validation") {
+    describe("Validated") {}
+  }
+}
